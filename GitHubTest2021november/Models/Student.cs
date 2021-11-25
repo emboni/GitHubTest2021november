515ecdf9e9e273cbad49a438e8695cda50222a37
Emil Bodholdt Nielsen <idvx3230@edu.zealand.dk>
2021-11-25 11:22:21 +0100

    Added student class

---
+++ GitHubTest2021november/Models/Student.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GitHubTest2021november.Models
+{
+    public class Student
+    {
+
+    }
+}
